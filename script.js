d51d211b76fb97bf22d06d894c2c1b6dfdb82622
Renegade <fleeceriddle@gmail.com>
2024-06-04 07:18:21 +0300

    updated script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+let userPetAge = 15;
+
+userPetAge++;
+
+console.log(userPetAge);
+
+
+console.log(10%3);
+
+
+console.log(5 * 5 - 20 === '5');
+
+const userPetMarried = true,
+      userPetIsParent = false;
+
+console.log(userPetMarried || userPetIsParent);
