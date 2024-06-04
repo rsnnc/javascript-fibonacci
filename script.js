03c6dec7e16362da0b271c3da0755eb564695594
Renegade <fleeceriddle@gmail.com>
2024-06-04 13:43:06 +0300

    gitignore specifications

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let userPetAge = 15;
+"use strict";
 
-userPetAge++;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
 
-console.log(userPetAge);
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const filmName1 = prompt('Один из последних просмотренных фильмов?', '');
+const filmRate1 = prompt('На сколько оцените его?', '');
+const filmName2 = prompt('Один из последних просмотренных фильмов?', '');
+const filmRate2 = prompt('На сколько оцените его?', '');
 
-console.log(10%3);
+personalMovieDb.movies[filmName1] = filmRate1;
+personalMovieDb.movies[filmName2] = filmRate2;
 
+console.log(personalMovieDb);
 
-console.log(5 * 5 - 20 === '5');
-
-const userPetMarried = true,
-      userPetIsParent = false;
-
-console.log(userPetMarried || userPetIsParent);
